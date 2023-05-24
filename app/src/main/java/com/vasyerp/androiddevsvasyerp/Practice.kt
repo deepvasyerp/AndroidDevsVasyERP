6faa709cd92d303963b28aa8628be2ae49b097f4
Shubham Diwakar <shubhamdiwakar@vasyerp.com>
2023-05-24 14:53:51 +0530

    new activity created

---
+++ app/src/main/java/com/vasyerp/androiddevsvasyerp/Practice.kt
@@ -0,0 +1,11 @@
+package com.vasyerp.androiddevsvasyerp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Practice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice)
+    }
+}
