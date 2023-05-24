9281bf43b2b2666e900ae40eb6b8d346225d84be
Abhishek Soni <abhisheksoni@vasyerp.com>
2023-05-24 12:03:41 +0530

    Signup Button Created

--- app/src/main/java/com/vasyerp/androiddevsvasyerp/MainActivity.kt
+++ app/src/main/java/com/vasyerp/androiddevsvasyerp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.vasyerp.androiddevsvasyerp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.signUpBtn)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
