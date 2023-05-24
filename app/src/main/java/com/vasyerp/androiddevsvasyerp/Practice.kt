d8ad9acb520391b4ad67acf8ba9b36bcc65b2c56
Shubham Diwakar <shubhamdiwakar@vasyerp.com>
2023-05-24 15:13:32 +0530

    toast added to signup btn

--- app/src/main/java/com/vasyerp/androiddevsvasyerp/Practice.kt
+++ app/src/main/java/com/vasyerp/androiddevsvasyerp/Practice.kt
@@ -2,13 +2,19 @@ package com.vasyerp.androiddevsvasyerp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class Practice : AppCompatActivity() {
+    lateinit var signupBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practice)
 
-
+        signupBtn=findViewById(R.id.signUpBtn)
+        signupBtn.setOnClickListener {
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
