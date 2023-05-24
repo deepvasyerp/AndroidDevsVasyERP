d2431c4277137f8613fb896639992c5a0c7044fe
deepvasyerp <deepmodi.vasyerp@gmail.com>
2023-05-24 11:10:23 +0530

    Initial commit.

--- app/src/main/java/com/vasyerp/androiddevsvasyerp/MainActivity.kt
+++ app/src/main/java/com/vasyerp/androiddevsvasyerp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.vasyerp.androiddevsvasyerp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ed_name: EditText
+    lateinit var ed_email:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ed_name=findViewById(R.id.edt_name)
+        ed_email=findViewById(R.id.ed_email)
+
+
     }
 }
